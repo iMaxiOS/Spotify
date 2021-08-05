dccf56cbffb5aff50b816875f1791ab7a55033e1
Macostik <y.granchenko@gmail.com>
2021-08-05 09:30:50 +0300

    create project

---
+++ Spotify_LBTA/Spotify_LBTA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify_LBTA
+//
+//  Created by Maxim Granchenko on 05.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
