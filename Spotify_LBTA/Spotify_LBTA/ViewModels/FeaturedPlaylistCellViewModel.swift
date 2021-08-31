6e1c3139b55cab12b3030a2ebb423c1724239b91
Macostik <y.granchenko@gmail.com>
2021-08-31 14:18:17 +0300

    featuredPlaylistCellViewModel and recommendedTrackCellViewModel

---
+++ Spotify_LBTA/Spotify_LBTA/ViewModels/FeaturedPlaylistCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedPlaylistCellViewModel.swift
+//  Spotify_LBTA
+//
+//  Created by Maxim Granchenko on 31.08.2021.
+//
+
+import Foundation
+
+struct FeaturedPlaylistCellViewModel {
+    let name: String
+    let artworkURL: URL?
+    let creatorName: String
+}
---
+++ Spotify_LBTA/Spotify_LBTA/ViewModels/RecommendedTrackCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecommendedTrackCellViewModel.swift
+//  Spotify_LBTA
+//
+//  Created by Maxim Granchenko on 31.08.2021.
+//
+
+import Foundation
+
+struct RecommendedTrackCellViewModel {
+    let name: String
+    let artistName: String
+    let artworkURL: URL?
+}
