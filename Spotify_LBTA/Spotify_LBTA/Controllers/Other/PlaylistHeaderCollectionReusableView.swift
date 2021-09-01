01fca7e1e079d149c915828216b309811e1f2c84
Macostik <y.granchenko@gmail.com>
2021-09-01 20:51:55 +0300

    added playlistDetailsHeader, share url link, fetchData playlist

---
+++ Spotify_LBTA/Spotify_LBTA/Controllers/Other/PlaylistHeaderCollectionReusableView.swift
@@ -0,0 +1,131 @@
+//
+//  PlaylistHeaderCollectionReusableView.swift
+//  Spotify_LBTA
+//
+//  Created by Maxim Granchenko on 01.09.2021.
+//
+
+import UIKit
+import SDWebImage
+
+protocol PlaylistHeaderCollectionReusableViewDelegate: AnyObject {
+    func playlistHeaderCollectionReusableViewDidTapPlayAll(_ header: PlaylistHeaderCollectionReusableView)
+}
+
+class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
+    static let identifier = "PlaylistHeaderCollectionReusableView"
+    
+    weak var delegate: PlaylistHeaderCollectionReusableViewDelegate?
+    
+    private let headerImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "photo")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private let ownerLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14, weight: .light)
+        label.textColor = .gray
+        return label
+    }()
+    
+    private let hStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.spacing = 5
+        stack.axis = .horizontal
+        stack.alignment = .bottom
+        return stack
+    }()
+    
+    private let vStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.spacing = 5
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        return stack
+    }()
+    
+    private let playAllButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(
+            systemName: "play.fill",
+            withConfiguration: UIImage.SymbolConfiguration(pointSize: 30, weight: .regular)
+        )
+        button.setImage(image, for: .normal)
+        button.backgroundColor = .systemGreen
+        button.tintColor = .label
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 30
+        button.addTarget(self, action: #selector(handlePlayAll), for: .touchUpInside)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        addSubview(headerImageView)
+        addSubview(vStackView)
+        
+        vStackView.addArrangedSubview(nameLabel)
+        vStackView.addArrangedSubview(descriptionLabel)
+        vStackView.addArrangedSubview(hStackView)
+        
+        hStackView.addArrangedSubview(ownerLabel)
+        hStackView.addArrangedSubview(playAllButton)
+        
+        setupHeaderImageView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+//MARK: - Extension
+extension PlaylistHeaderCollectionReusableView {
+    private func setupHeaderImageView() {
+        NSLayoutConstraint.activate([
+            headerImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            headerImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            headerImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.6),
+            headerImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.6),
+            
+            playAllButton.heightAnchor.constraint(equalToConstant: 60),
+            playAllButton.widthAnchor.constraint(equalToConstant: 60),
+            
+            vStackView.topAnchor.constraint(equalTo: headerImageView.bottomAnchor, constant: 5),
+            vStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            vStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16),
+            vStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+        ])
+    }
+    
+    public func configure(with model: Playlist) {
+        nameLabel.text = model.name
+        descriptionLabel.text = model.description
+        ownerLabel.text = model.owner.display_name
+        headerImageView.sd_setImage(with: URL(string: model.images.first?.url ?? ""))
+    }
+    
+    @objc private func handlePlayAll() {
+        self.delegate?.playlistHeaderCollectionReusableViewDidTapPlayAll(self)
+    }
+}
--- Spotify_LBTA/Spotify_LBTA/Controllers/Other/PlaylistViewController.swift
+++ Spotify_LBTA/Spotify_LBTA/Controllers/Other/PlaylistViewController.swift
@@ -9,7 +9,43 @@ import UIKit
 
 class PlaylistViewController: UIViewController {
 
+    private var viewModels: [RecommendedTrackCellViewModel] = []
     private let playlist: Playlist
+    
+    private let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _, _ in
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+            )
+            
+            item.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 0, bottom: 1, trailing: 0)
+            
+            let size = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(60)
+            )
+            
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitem: item, count: 1)
+            let section = NSCollectionLayoutSection(group: group)
+            
+            section.boundarySupplementaryItems = [
+                NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .fractionalWidth(1)
+                    ),
+                    elementKind: UICollectionView.elementKindSectionHeader,
+                    alignment: .top
+                )
+            ]
+            
+            return section
+        })
+    )
     
     init(playlist: Playlist) {
         self.playlist = playlist
@@ -24,24 +60,130 @@ class PlaylistViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        setupCollectionView()
         fetchData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
     }
 }
 
 extension PlaylistViewController {
     private func setupView() {
         title = playlist.name
-        navigationItem.largeTitleDisplayMode = .never
         view.backgroundColor = .systemBackground
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(handleShare)
+        )
+    }
+    
+    private func setupCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(
+            PlaylistHeaderCollectionReusableView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: PlaylistHeaderCollectionReusableView.identifier
+        )
+        collectionView.register(
+            RecommendedTrackCollectionViewCell.self,
+            forCellWithReuseIdentifier: RecommendedTrackCollectionViewCell.identifier
+        )
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     private func fetchData() {
-        APICaller.shared.getPlaylistDetail(for: playlist) { result in
-            switch result {
-            case .success(let model): break
-            case .failure(let error): break
+        APICaller.shared.getPlaylistDetail(for: playlist) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let model):
+                    
+                    self?.viewModels = model.tracks.items.compactMap {
+                        return RecommendedTrackCellViewModel(
+                            name: $0.track.name,
+                            artistName: $0.track.artists.first?.name ?? "",
+                            artworkURL: URL(string: $0.track.album?.images.first?.url ?? "")
+                        )
+                    }
+                    self?.collectionView.reloadData()
+                case .failure(let error):
+                    print("🍎🍎🍎\(error.localizedDescription)")
+                }
             }
         }
     }
+    
+    @objc private func handleShare() {
+        guard let url = URL(string: playlist.external_urls["spotify"] ?? "") else {
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [url], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 }
 
+//MARK: - Data Source
+extension PlaylistViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: RecommendedTrackCollectionViewCell.identifier,
+                for: indexPath
+        ) as? RecommendedTrackCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.configureCell(with: viewModels[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        viewForSupplementaryElementOfKind kind: String,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: PlaylistHeaderCollectionReusableView.identifier,
+            for: indexPath
+        ) as? PlaylistHeaderCollectionReusableView else {
+            return UICollectionReusableView()
+        }
+        
+        header.configure(with: playlist)
+        header.delegate = self
+        
+        return header
+    }
+}
+
+//MARK: - Delegate
+extension PlaylistViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("🍎🍎🍎")
+        #warning("do push")
+    }
+}
+
+//MARK: - PlaylistHeaderCollectionReusableView Delegate
+extension PlaylistViewController: PlaylistHeaderCollectionReusableViewDelegate {
+    
+    func playlistHeaderCollectionReusableViewDidTapPlayAll(_ header: PlaylistHeaderCollectionReusableView) {
+        print("🍎🍎🍎 Start play list all")
+        #warning("Start play list all queue")
+    }
+}
